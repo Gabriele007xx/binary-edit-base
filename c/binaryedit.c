cee8177c3ae501dbd7b2f271d343c2cbd17dd135
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-11 18:47:55 +0200

    Update binaryedit.c

--- c/binaryedit.c
+++ c/binaryedit.c
@@ -1,1 +1,36 @@
 
+#include<stdlib.h>
+#include<stdio.h>
+
+int main() {
+  FILE *fd;
+  int x=12;
+
+			/* apre il file */
+  fd=fopen("test.dat", "w");
+  if( fd==NULL ) {
+    perror("Errore in apertura del file");
+    exit(1);
+  }
+
+
+			/* scrive il numero */
+  fwrite(&x, sizeof(int), 1, fd);
+
+
+			/* incrementa e scrive */
+  x++;
+  fwrite(&x, sizeof(int), 1, fd);
+
+
+
+			/* decrementa e scrive */
+  x-=2;
+  fwrite(&x, sizeof(int), 1, fd);
+
+
+			/* chiude il file */
+  fclose(fd);
+
+  return 0;
+}
